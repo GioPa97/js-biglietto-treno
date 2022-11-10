188939b8b6ac767be897fe600ddaa4cf348b486f
GioPa97 <giovannipagliara97@gmail.com>
2022-11-10 15:33:34 +0100

    Tariffa base

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
 let valorepercorrenza = prompt("Inserisci il numero di Km da percorrere");
 let etapasseggero = prompt("Inserisci l'età del passeggero");
 
-console.log("Hello world!");
+let tariffabase = 0.21;
+
+let tariffacalcolata = valorepercorrenza * tariffabase;
+
+let scontominorenni = (tariffacalcolata*20/100);
+let scontoanziani = (tariffacalcolata*40/100);
+
+console.log(
+    `
+    Informazioni___________________________________________________
+
+        1)Km da percorrere:      ${valorepercorrenza} Km
+        2)tariffa base:          ${tariffabase} €
+        )Età passeggero:        ${etapasseggero} Anni
+        
+    
+
+        Prezzo finale:          €
+
+    ____________________________________________________Informazioni
+    `
+);
+
+
+
+    console.log(
+
+     `
+    ${tariffacalcolata}
+    
+  
+
+    `
+     );
+
+
+
+
+
+
+
+
+
+
+/*
+document.getElementById("p-nome").innerHTML = nome;
+
+document.getElementById("p-prezzo").innerHTML = nome + cognome + colore + (numero1 / numero2); */
